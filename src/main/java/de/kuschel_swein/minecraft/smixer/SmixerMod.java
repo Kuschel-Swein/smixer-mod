9a225667654224e89a42d1c4493e4ced677c2e97
Kuschel_Swein <kuschelswein@gmail.com>
2023-06-30 21:11:14 +0200

    enforce format

--- src/main/java/de/kuschel_swein/minecraft/smixer/SmixerMod.java
+++ src/main/java/de/kuschel_swein/minecraft/smixer/SmixerMod.java
@@ -9,8 +9,11 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
         version = SmixerMod.VERSION
 )
 public class SmixerMod {
-        public static final String MOD_ID = "smixer";
-    public static final String  MOD_NAME = "Smixer";
+
+    public static final String MOD_ID = "smixer";
+
+    public static final String MOD_NAME = "Smixer";
+
     public static final String VERSION = "1.0";
 
 
