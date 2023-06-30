5a5aca3fec560e6aad4b5d2c6a51ccd2ee3aba19
Kuschel_Swein <kuschelswein@gmail.com>
2023-06-30 21:53:00 +0200

    remove empty mixin

--- src/main/java/de/kuschel_swein/minecraft/smixer/mixins/sba/SkyblockAddonsMixin.java
+++
@@ -1,9 +0,0 @@
-package de.kuschel_swein.minecraft.smixer.mixins.sba;
-
-import codes.biscuit.skyblockaddons.SkyblockAddons;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(SkyblockAddons.class)
-public class SkyblockAddonsMixin {
-
-}
