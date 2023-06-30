102402046b798dc60d4103fc111cb072ee982b7e
Kuschel_Swein <kuschelswein@gmail.com>
2023-06-30 21:59:53 +0200

    correct version in main class

--- src/main/java/de/kuschel_swein/minecraft/smixer/SmixerMod.java
+++ src/main/java/de/kuschel_swein/minecraft/smixer/SmixerMod.java
@@ -14,7 +14,7 @@ public class SmixerMod {
 
     public static final String MOD_NAME = "Smixer";
 
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.0";
 
 
     @Mod.EventHandler
