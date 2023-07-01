697b81747bf35df5e5838afbb3641689bd014ba0
Kuschel_Swein <kuschelswein@gmail.com>
2023-07-01 20:02:04 +0200

    show settings button for replaced features always

--- src/main/java/de/kuschel_swein/minecraft/smixer/mixins/sba/gui/SkyblockAddonsGuiMixin.java
+++ src/main/java/de/kuschel_swein/minecraft/smixer/mixins/sba/gui/SkyblockAddonsGuiMixin.java
@@ -1,16 +1,23 @@
 package de.kuschel_swein.minecraft.smixer.mixins.sba.gui;
 
+import codes.biscuit.skyblockaddons.SkyblockAddons;
 import codes.biscuit.skyblockaddons.core.Feature;
 import codes.biscuit.skyblockaddons.gui.SkyblockAddonsGui;
+import codes.biscuit.skyblockaddons.gui.buttons.ButtonSettings;
+import codes.biscuit.skyblockaddons.utils.EnumUtils;
 import de.kuschel_swein.minecraft.smixer.abstraction.smixer.ReplacementFeature;
+import net.minecraft.client.gui.GuiScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.TreeSet;
 
 @Mixin(SkyblockAddonsGui.class)
-public class SkyblockAddonsGuiMixin {
+public abstract class SkyblockAddonsGuiMixin extends GuiScreen {
 
     @Redirect(
             method = "initGui",
@@ -35,5 +42,47 @@ public class SkyblockAddonsGuiMixin {
         }
 
         return false;
+    }
+
+    @Inject(
+            method = "addButton",
+            locals = LocalCapture.CAPTURE_FAILHARD,
+            at = @At(
+                    value = "INVOKE",
+                    target = "Ljava/util/List;add(Ljava/lang/Object;)Z",
+                    shift = At.Shift.BEFORE,
+                    ordinal = 3,
+                    remap = false
+            ),
+            remap = false
+    )
+    private void injectReplacementFeatureToAddButton(
+            Feature feature,
+            EnumUtils.ButtonType buttonType,
+            CallbackInfo callback,
+            String text,
+            int halfWidth,
+            int boxWidth,
+            int boxHeight,
+            int x,
+            double y
+    ) {
+        ReplacementFeature replacementFeature = ReplacementFeature.forFeature(feature);
+
+        if (replacementFeature != null) {
+            // we need the settings button even if there are no settings as we use it as info button
+            if (feature.getSettings().isEmpty()) {
+                this.buttonList.add(
+                        new ButtonSettings(
+                                (x + boxWidth - 33),
+                                (y + boxHeight - 20),
+                                text,
+                                SkyblockAddons.getInstance(),
+                                feature
+                        )
+                );
+            }
+        }
+
     }
 }
